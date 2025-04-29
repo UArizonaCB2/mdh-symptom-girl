2bad31d8e5bed2b6385666c35c4e96fe8979bc65
Shravan Aras <123.shravan@gmail.com>
2025-04-29 08:47:01 -0700

    Updated JSON to reflect the new symptoms
    1. Updated the JSON to reflect the new symptoms.
    2. Updated size of symptom container to fit the longer list of symptoms.

--- script.js
+++ script.js
@@ -3,15 +3,41 @@
 // NOTE: This is not the full JSON. Will be updated once symptoms are finalized.
 const symptomMap = {
     "region1": [
-        { "value": "fatigue", "text": "Fatigue" },
+        { "value": "forgetfulness", "text": "Forgetfulness"},
+        { "value": "dizziness_fainting ", "text": "Dizziness/Fainting" },
+        { "value": "headache", "text": "Headache", "critical": "yes"},
         { "value": "snoring", "text": "Snoring" },
-        { "value": "poor_sleep", "text": "Poor Sleep" },
-        { "value": "headache", "text": "Headache" }
+        { "value": "nausea", "text": "Nausea/Vomiting/Reflux"},
+        { "value": "visual_changes", "text": "Visual Changes", "critical": "yes"},
+        { "value": "fever", "text": "Fever", "critical": "yes"},
     ],
     "region2": [
-        { "value": "vomiting", "text": "Vomiting" },
-        { "value": "dry_mouth", "text": "Dry Mouth" },
-        { "value": "smell_change", "text": "Smell Change" }
+        { "value": "heart_palpitations", "text": "Heart Palpitations", "critical": "yes"},
+        { "value": "shortness_of_breath", "text": "Shortness Of Breath" },
+        { "value": "swollen_hands", "text": "Swollen Hands"},
+        { "value": "sore_nipples", "text": "Sore Nipples" },
+        { "value": "colostrum", "text": "Colostrum/Milk" },
+        { "value": "fatigue", "text": "Fatigue" },
+        { "value": "itchy_skin", "text": "Itchy Skin" },
+        { "value": "contractions", "text": "Contractions", "critical":"yes"},
+    ],
+    "region3": [
+        { "value": "hip_pelvic_vaginal_pain", "text": "Hip/Pelvic/Vaginal Pain"},
+        { "value": "vaginal_discharge", "text": "Vaginal Discharge/Itching"},
+        { "value": "constipation", "text": "Constipation"},
+        { "value": "urinary_frequency", "text": "Unirary Frequency"},
+        { "value": "bleeding_spotting", "text": "Bleeding/Spotting", "critical": "yes"},
+        { "value": "leaking_fluid", "text": "Leaking Fluid", "critical": "yes"},
+        { "value": "change_in_sex_drive", "text": "Change in Sex Drive"},
+    ],
+    "region4": [
+        { "value": "new_stretch_marks", "text": "New Stretch Marks"},
+        { "value": "swollen_feet", "text": "Swollen Feet"},
+        { "value": "varicose_veins", "text": "Varicose Veins"},
+        { "value": "restless_legs", "text": "Restless Legs"},
+        { "value": "back_pain", "text": "Back Pain"},
+        { "value": "leg_cramps", "text": "Leg Cramps"},
+        { "value": "sciatica", "text": "Sciatica"},
     ]
 };
 
